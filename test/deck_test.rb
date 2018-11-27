7f3e74083f1a7959f0a9ca348e35712c6737049a
Teresa Knowles <teresa07moreno@gmail.com>
2018-11-26 21:30:23 -0700

    Adds count method to deck and tests it

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -18,9 +18,20 @@ class DeckTest < MiniTest::Test
 
     assert_instance_of Deck, deck
     refute cards.empty?
-
-    binding.pry
   end
+
+  def test_it_can_count_the_cards_in_deck
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal 3, deck.count
+
+  end 
+
 
 
 end
