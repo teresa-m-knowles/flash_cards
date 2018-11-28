598f3d57b52d5b0c8da82c08d32fe5cc533e0b80
Teresa Knowles <teresa07moreno@gmail.com>
2018-11-28 15:09:50 -0700

    Adds test that tests if it can evaluate the guess of a new turn object

--- test/round_test.rb
+++ test/round_test.rb
@@ -72,6 +72,22 @@ class RoundTest < MiniTest::Test
 
     assert_instance_of Turn, new_turn
 
+    assert_equal new_turn.class, Turn
+
+  end
+
+  def test_it_can_evaluate_the_answer_of_a_new_turn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    new_turn = round.take_turn("Juneau")
+
+    assert new_turn.correct?
   end
 
 
