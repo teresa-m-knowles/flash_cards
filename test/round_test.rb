3d26e91c54d5c14761e9a4bf138278be34b9e620
Teresa Knowles <teresa07moreno@gmail.com>
2018-11-28 14:58:30 -0700

    Adds new test to check that a new round object has an empty turns array

--- test/round_test.rb
+++ test/round_test.rb
@@ -20,11 +20,31 @@ class RoundTest < MiniTest::Test
 
     assert_instance_of Round, round
 
-    binding.pry
-
   end
 
-  # def test_it_)
-  # end
+  def test_it_can_access_the_deck_in_a_round
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_equal round.deck, deck
+  end
+
+  def test_a_new_round_object_has_an_empty_turns_array_when_initialized
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_empty round.turns
+  end
+
 
 end
