25c0f91e48082e447471800c4dad9a9b87c5d6aa
Teresa Knowles <teresa07moreno@gmail.com>
2018-11-28 15:06:48 -0700

    Adds test for the take_turn method. It tests that, when called, it creates an instance of the Turn class

--- test/round_test.rb
+++ test/round_test.rb
@@ -47,7 +47,7 @@ class RoundTest < MiniTest::Test
   end
 
   def test_it_shows_the_first_card_as_the_current_card
-    
+
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -58,6 +58,23 @@ class RoundTest < MiniTest::Test
 
     assert_equal cards[0], round.current_card
   end
+
+  def test_that_it_can_take_a_turn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    new_turn = round.take_turn("Juneau")
+
+    assert_instance_of Turn, new_turn
+
+  end
+
+
 
 
 end
