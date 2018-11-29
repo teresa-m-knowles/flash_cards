8ae5b8a65785bcccb6a4e45f08cc18e9b4f0501d
Teresa Knowles <teresa07moreno@gmail.com>
2018-11-29 13:18:27 -0700

    Added test to see if percent methods work

--- test/round_test.rb
+++ test/round_test.rb
@@ -154,4 +154,32 @@ def test_it_can_count_turns_and_get_feedback_from_turn
 
 
   end
+
+  def test_that_it_can_figure_out_the_number_of_correct_answers_by_category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    new_turn = round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+
+    assert_equal 1, round.number_correct_by_category(:Geography)
+
+
+    assert_equal 0, round.number_correct_by_category(:STEM)
+
+    assert_equal 50, round.percent_correct
+
+    assert_equal 100,  round.percent_correct_by_category(:Geography)
+
+    assert_equal cards[2], round.current_card
+
+
+  end
+
 end
