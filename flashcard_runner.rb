ce17d047191209c32560d01f8faf3082ea95849f
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-01 14:03:22 -0700

    Modifies runner file to create a new round with the return value (a deck object) from the start method of the card generator class

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -6,7 +6,5 @@ require './lib/cardgenerator'
 require 'pry'
 
 new_generator = CardGenerator.new('cards.txt')
-new_set_of_cards_from_file = new_generator.cards
-new_deck = Deck.new(new_set_of_cards_from_file)
-round = Round.new(new_deck)
+round = Round.new(new_generator.cards)
 round.start
