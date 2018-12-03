90e11cc9d95dd1e993cabe0faa170be302889761
Teresa Knowles <teresa07moreno@gmail.com>
2018-12-02 21:18:02 -0700

    Modifies cards method to use map method instead of each

--- lib/cardgenerator.rb
+++ lib/cardgenerator.rb
@@ -11,11 +11,11 @@ class CardGenerator
   end
 
   def cards
-    @cards = []
-    @file.readlines.each do |line|
+
+
+    @cards = @file.readlines.map do |line|
       card = line.split(',')
-      new_card = Card.new(card[0], card[1], card[2])
-      @cards << new_card
+      Card.new(card[0], card[1], card[2])
     end
     Deck.new(@cards)
   end
